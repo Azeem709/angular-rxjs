1ead248bc0ca6560b2f7125f838eee57d255764d
Azeem709 <azeemrahman5@gmail.com>
2021-03-06 19:44:25 +0530

    updated readme

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # angular-27gwhd
 
+// creational Operator
+  /**
+   * Operators : of,from
+   * from - can be used when input is array's or iterables or promises
+   * of - it emits each object where as of operator will print the arraty as declerative
+   */
+  creationalOperator() {
+    let fromObservable = from(this.testArray);
+    let ofObservable = of(this.testArray);
+    ofObservable.subscribe(res => console.log(res)); 
+    // return ["Value1", "value2", "value3"]
+
+    fromObservable.subscribe(res => console.log(res)); // value1 value2 value3
+  }
+
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-27gwhd)
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { from, of } from "rxjs";
+import { filter } from "rxjs/operators";
 
 @Component({
   selector: "my-app",
@@ -8,21 +9,37 @@ import { from, of } from "rxjs";
 })
 export class AppComponent implements OnInit {
   name = "RXJS TUTORIAL";
+  testArray = ["Value1", "value2", "value3"];
+
+  ngOnInit(): void {
+    this.creationalOperator();
+    this.nonCreationalOperator();
+  }
 
   // creational Operator
   /**
+   * Operators : of,from
    * from - can be used when input is array's or iterables or promises
-   * it emits each object where as of operator will print the arraty as declerative
+   * of - it emits each object where as of operator will print the arraty as declerative
    */
   creationalOperator() {
-    let testArray = ["Value1", "value2", "value3"];
-    let ofObservable = from(testArray);
-    let fromObservable = of(testArray);
+    let fromObservable = from(this.testArray);
+    let ofObservable = of(this.testArray);
     ofObservable.subscribe(res => console.log(res));
     fromObservable.subscribe(res => console.log(res));
   }
 
-  ngOnInit(): void {
-    this.creationalOperator();
+
+/**
+ * Pipe  - operator is used combine multiple rxjs operators(other than creational) to form a data pipeline
+ * it is considered best practise to use the pipe operator even though we are using single operator
+ * 
+ * filter - operator takes a predicate function as input and retuns filtered observable 
+ * 
+ */
+  nonCreationalOperator(){
+       let filterOperator = from(this.testArray);
+       filterOperator.pipe(filter(op => op == "value2"))
+            .subscribe(res => console.log(res))
   }
 }
