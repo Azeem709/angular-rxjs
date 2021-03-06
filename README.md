c756a6cf0ccd3d9e85ced22f926c25a9fc4755fb
Azeem709 <azeemrahman5@gmail.com>
2021-03-06 21:06:55 +0530

    3 files modified

--- README.md
+++ README.md
@@ -1,5 +1,22 @@
 # angular-rxjs
 
+CheatSheet : 
+
+Observable : Observables are unicast( where each observer own it own independent execution context)
+
+Subject : are special type of Observables which are multicasted(lile event emitter contains the registery of listeners).Useful when want to emit values to multiple observers.
+
+Special Variant of Subject - 
+
+BehaviourSubject : maintains/persist the last emitted value which observer can subscribe though
+
+ReplaySubject : maintain list of last emitted values (will have to provide the buffer how many we want it to maintain)
+
+AsynSubject : Its like BehaviourSubject subject it emits last value to observers but it emits only when the subject is completed ( when complete() call back is called)
+
+
+
+
 // creational Operator
   /**
    * Operators : of,from
@@ -27,5 +44,6 @@
  * filter - operator takes a predicate function as input and retuns filtered observable 
  * 
  */
+
 
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-27gwhd)
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from "@angular/core";
-import { from, of } from "rxjs";
-import { filter } from "rxjs/operators";
+import { from, Observable, of, pipe } from "rxjs";
+import { filter, first, map } from "rxjs/operators";
 
 @Component({
   selector: "my-app",
@@ -9,11 +9,30 @@ import { filter } from "rxjs/operators";
 })
 export class AppComponent implements OnInit {
   name = "RXJS TUTORIAL";
-  testArray = ["Value1", "value2", "value3"];
+  testArray = ["value1", "value1", "value2", "value3", "value2"];
 
   ngOnInit(): void {
+    this.basicObservableCreation();
     this.creationalOperator();
-    this.nonCreationalOperator();
+    this.pipeableOperator();
+  }
+
+  /**
+   * Creation through Observable
+   * or can be created from the creational operators
+   */
+  basicObservableCreation() {
+    const foo = new Observable(subscriber => {
+      subscriber.next(42);
+      subscriber.next(100);
+      subscriber.next(200);
+      setTimeout(() => {
+        subscriber.next(300); // happens asynchronously
+      }, 1000);
+      subscriber.complete();
+      subscriber.next(4); // Is not delivered because it would violate the contract
+    });
+    foo.subscribe(res => console.log(res));
   }
 
   // creational Operator
@@ -29,17 +48,37 @@ export class AppComponent implements OnInit {
     fromObservable.subscribe(res => console.log(res));
   }
 
+  /**
+   *  "A Pipeable Operator is a function that takes an Observable as its input and returns another Observable. It is a pure operation: the previous Observable stays unmodified."
+   *
+   * Pipe  - operator is used combine multiple rxjs operators(other than creational) to form a data pipeline
+   * it is considered best practise to use the pipe operator even though we are using single operator
+   *
+   *
+   * filter - operator takes a predicate function as input and retuns filtered observable
+   *
+   *
+   * first - operator return the first element
+   *       - also takes predicate function as input can be useful while we just single single from the observable
+   */
+  pipeableOperator() {
+    let filterOperator = from(this.testArray);
+    filterOperator
+      .pipe(filter(op => op == "value2"))
+      .subscribe(res => console.log(res));
 
-/**
- * Pipe  - operator is used combine multiple rxjs operators(other than creational) to form a data pipeline
- * it is considered best practise to use the pipe operator even though we are using single operator
- * 
- * filter - operator takes a predicate function as input and retuns filtered observable 
- * 
- */
-  nonCreationalOperator(){
-       let filterOperator = from(this.testArray);
-       filterOperator.pipe(filter(op => op == "value2"))
-            .subscribe(res => console.log(res))
+    let firstOperator = from(this.testArray);
+    firstOperator.pipe(first()).subscribe(res => console.log(res)); //value1
+    let firstPredicateObservale = from(this.testArray);
+    firstPredicateObservale
+      .pipe(first(val => val == "value2"))
+      .subscribe(res => console.log(res));
+
+    // map
+    let numsObserver = from([1, 2, 3, 4]);
+      numsObserver.pipe(
+        map(val => val*val )
+      )
+      .subscribe(res=> console.log(res))
   }
 }
